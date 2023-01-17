72767cd78f1892f1bbad3cd1a981a0b159d13b0c
AngeCk98 <Angekoubangou@gmail.com>
2023-01-16 23:24:35 -0800

    pall opcode

---
+++ o_pall.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * o_pall - prints all data in the stack
+ * @top: top of the stack
+ * @line_number: currect line number at which command is
+ */
+void o_pall(stack_t **top, __attribute__((unused))unsigned int line_number)
+{
+	stack_t *temp = *top;
+
+	if (temp == NULL)
+		return;
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
